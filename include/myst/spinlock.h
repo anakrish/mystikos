970c919183fbe1b94f7ec8d2b8c9af6c4b46a0ce
Anand Krishnamoorthi <anakrish@microsoft.com>
2022-03-14 11:36:50 +0530

    Owner
    Signed-off-by: Anand Krishnamoorthi <anakrish@microsoft.com>

--- include/myst/spinlock.h
+++ include/myst/spinlock.h
@@ -12,6 +12,7 @@ typedef struct myst_spinlock
 {
     volatile uint16_t ticket;
     volatile uint16_t serving;
+    volatile void* owner;
 } myst_spinlock_t;
 
 #define MYST_SPINLOCK_INITIALIZER \
@@ -23,18 +24,24 @@ MYST_INLINE void myst_spinlock_init(myst_spinlock_t* s)
 {
     s->ticket = 0;
     s->serving = 0;
+    s->owner = 0;
 }
 
 MYST_INLINE void myst_spin_lock(myst_spinlock_t* s)
 {
+    void* thread;
+    asm volatile("mov %%fs, %0" : "=r"(thread));
+
     int my_ticket = __atomic_fetch_add(&s->ticket, 1, __ATOMIC_ACQ_REL);
     while (__atomic_load_n(&s->serving, __ATOMIC_ACQUIRE) != my_ticket)
         asm volatile("pause" ::: "memory");
+    s->owner = thread;
 }
 
 MYST_INLINE void myst_spin_unlock(myst_spinlock_t* s)
 {
     __atomic_fetch_add(&s->serving, 1, __ATOMIC_RELEASE);
+    s->owner = NULL;
 }
 
 #endif /* _MYST_SPINLOCK_H */
